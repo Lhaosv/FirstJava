49c8a85c8a0ddfc8cd673f0b2ddad7a0821f012d
Lhaosv <61543316+Lhaosv@users.noreply.github.com>
2020-11-08 16:41:44 +0800

    泛型方法示例

---
+++ TestHello.java
@@ -0,0 +1,25 @@
+package com.db.stu;
+import java.util.ArrayList;
+
+public class TestHello {
+	public static <T> void DisplayArray(T[] anArray){
+		for(T t:anArray)
+		{
+			System.out.println(t);
+			System.out.println("----");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String strArray []={"ABC","DEF","HIJ"};
+		DisplayArray(strArray);
+		Integer intArray []={1,2,3,4,5};
+		DisplayArray(intArray);
+		
+		
+		
+
+	}
+
+}
